28fef8df3f907e7f7021bbc833c003bbd9326c5b
Josué Hoenicka <josuee.vzla@gmail.com>
2023-02-03 06:46:39 -0300

    update: day 17

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { ContactReactiveComponent } from "./contact-reactive/contact-reactive.component";
 import { ContactComponent } from "./contact/contact.component";
+import { PermissionsGuard } from "./guards/permissions.guard";
+import { WithoutsaveGuard } from "./guards/withoutsave.guard";
 import { HomeComponent } from "./home/home.component";
 import { PagenotfoundComponent } from "./pagenotfound/pagenotfound.component";
 import { DetailsComponent } from "./users/details/details.component";
@@ -10,10 +12,11 @@ import { UserComponent } from "./users/user/user.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full'},
-  { path: 'contact-reactive', component: ContactReactiveComponent },
+  { path: 'contact-reactive', component: ContactReactiveComponent, canDeactivate:[WithoutsaveGuard] },
   { path: 'contact-template/:id', component: ContactComponent },
   { path: 'home', component: HomeComponent },
-  { path: 'users', component: UserComponent, children:[
+  { path: 'users', component: UserComponent, canActivate: [PermissionsGuard],
+    children:[
       { path: 'list', component: ListComponent },
       { path: 'details', component: DetailsComponent }]
   },
---
+++ src/app/guards/permissions.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PermissionsGuard implements CanActivate {
+  canActivate(): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (this.hasUser()){
+      return true;
+    }
+    // Redict login
+    alert('You do not permissions')
+    return false;
+  }
+
+  hasUser(): boolean {
+    return false;
+  }
+
+}
